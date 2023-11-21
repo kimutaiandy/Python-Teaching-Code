24a7f5644b243dc0f6e08b47848afc4395d50b73
Andrew Kimutai <61423343+kimutaiandy@users.noreply.github.com>
2023-11-21 11:45:54 +0300

    Update

--- pingpong2.py
+++ pingpong2.py
@@ -36,14 +36,14 @@ def draw(win, paddles):
     pygame.display.update()
 
 def handle_paddle_movement(keys, left_paddle, right_paddle):
-    if keys[pygame.K_w]:
+    if keys[pygame.K_w] and left_paddle.y - left_paddle.VEL >=0:
         left_paddle.move(up=True)
-    if keys[pygame.K_s]:
+    if keys[pygame.K_s] and left_paddle.y + left_paddle.VEL + left_paddle.height <= HEIGHT:
         left_paddle.move(up=False)
 
-    if keys[pygame.K_UP]:
+    if keys[pygame.K_UP] and right_paddle.y - right_paddle.VEL >=0:
         right_paddle.move(up=True)
-    if keys[pygame.K_DOWN]:
+    if keys[pygame.K_DOWN] and right_paddle.y + right_paddle.VEL + right_paddle.height <= HEIGHT:
         right_paddle.move(up=False)
 
 
