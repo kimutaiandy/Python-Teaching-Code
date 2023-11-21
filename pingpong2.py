6f274acc9081162b97b39c84e41a4a0da1273f62
Andrew Kimutai <61423343+kimutaiandy@users.noreply.github.com>
2023-11-21 12:17:35 +0300

    class ball updated

--- pingpong2.py
+++ pingpong2.py
@@ -27,6 +27,22 @@ class Paddle: #defining the paddle
         else:
             self.y += self.VEL
 
+class Ball:
+    MAX_VEL = 5
+    def __init__(self, x,y, radius):
+        self.x = x
+        self.y = y
+        self.radius = radius
+        self.x_vel = MAX_VEL
+        self.y_vel = 0
+    def draw(self, win):
+        pygame.draw.circle(win, self.COLOR, (self.x,self.y),self.radius)
+    def move(self):
+        self.x += self.x.vel
+        self.y += self.y.vel
+
+
+
 def draw(win, paddles):
     win.fill(BLACK)
 
