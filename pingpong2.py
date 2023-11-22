8528707d4d650048252f1a74b6dd632b036d9bf8
Andrew Kimutai <61423343+kimutaiandy@users.noreply.github.com>
2023-11-22 12:06:49 +0300

    Update pingpong2.py

--- pingpong2.py
+++ pingpong2.py
@@ -34,8 +34,8 @@ class Ball:
     MAX_VEL = 5
     COLOR = WHITE
     def __init__(self, x,y, radius):
-        self.x = x
-        self.y = y
+        self.x = self.original_x = x
+        self.y = self.original_y = y
         self.radius = radius
         self.x_vel = self.MAX_VEL
         self.y_vel = 0
@@ -47,7 +47,12 @@ class Ball:
     def move(self):
         self.x += self.x_vel
         self.y += self.y_vel
-
+        
+    def reset(self):
+        self.x = self.original_x
+        self.y = self.original_y
+        self.y_vel = 0
+        self.x_vel *= -1
 
 
 def draw(win, paddles, ball, left_score, right_score):
