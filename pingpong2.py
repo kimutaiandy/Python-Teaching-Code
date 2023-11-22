1ae73ee8499ab261bd3f2f5f49f0173d09b71e25
Andrew Kimutai <61423343+kimutaiandy@users.noreply.github.com>
2023-11-22 10:43:33 +0300

    Update pingpong2.py

--- pingpong2.py
+++ pingpong2.py
@@ -71,11 +71,23 @@ def handle_collision(ball, left_paddle,right_paddle):
         if ball.y >= left_paddle.y and ball.y <= left_paddle.y + left_paddle.height:
             if ball.x - ball.radius <= left_paddle.x + left_paddle.width:
                 ball.x_vel *= -1
+
+                middle_y = left_paddle.y + left_paddle.height / 2
+                difference_in_y = middle_y - ball.y
+                reduction_factor = (left_paddle.height/2) / ball.MAX_VEL
+                y_vel = difference_in_y /reduction_factor
+                ball.y_vel = -1* y_vel
+
     else:
         if ball.y >= right_paddle.y and ball.y <= right_paddle.y + right_paddle.height:
             if ball.x + ball.radius >= right_paddle.x:
                 ball.x_vel *= -1
 
+                middle_y = right_paddle.y + right_paddle.height / 2
+                difference_in_y = middle_y - ball.y
+                reduction_factor = (right_paddle.height/2) / ball.MAX_VEL
+                y_vel = difference_in_y /reduction_factor
+                ball.y_vel = -1* y_vel
 
 def handle_paddle_movement(keys, left_paddle, right_paddle):
     if keys[pygame.K_w] and left_paddle.y - left_paddle.VEL >=0:
