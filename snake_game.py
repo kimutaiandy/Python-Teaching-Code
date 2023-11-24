633b22243e095b7c8730443e197c3c23ce8f71a8
Andrew Kimutai <61423343+kimutaiandy@users.noreply.github.com>
2023-11-24 11:57:53 +0300

    Update snake_game.py

--- snake_game.py
+++ snake_game.py
@@ -7,15 +7,18 @@ pygame.init() #initializes pygame modules
 SW, SH = 800,800
 
 BLOCK_SIZE = 50
-FONT = pygame.font.Font("font.ttf", 100)
+FONT = pygame.font.Font("font.ttf", BLOCK_SIZE*2)
 
 screen = pygame.display.set_mode((800,800))
 pygame.display.set_caption("Snake!")
 clock = pygame.time.Clock()
 
 def drawGrid():
-    for x in range(0, SW)
-
+    for x in range(0, SW, BLOCK_SIZE):
+        for y in range(0, SH, BLOCK_SIZE):
+            rect = pygame.Rect(x,y, BLOCK_SIZE, BLOCK_SIZE)
+            pygame.draw.rect(screen, "#FFFFFF", rect, 1)
+drawGrid()
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
