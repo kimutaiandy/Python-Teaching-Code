12d30d5a43ecda7aa153a3a8ab8415d14db18676
Andrew Kimutai <61423343+kimutaiandy@users.noreply.github.com>
2023-11-24 09:38:59 +0300

    Create snake_game.py

---
+++ snake_game.py
@@ -0,0 +1,21 @@
+import pygame #imports pygame libraries
+import random #generates random actions
+import sys #provides functions and variables
+
+pygame.init() #initializes pygame modules
+
+SW, SH = 800,800
+
+FONT = pygame.font.Font("font.TFF", 100)
+
+screen = pygame.display.set_mode((800,800))
+pygame.display.set_caption("Snake!")
+clock = pygame.time.Clock()
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+    pygame.display.update()
+    clock.tick(10)
