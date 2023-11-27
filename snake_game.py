94153c3cc77b4a9a89538653f94a2eaeaefd2892
Andrew Kimutai <61423343+kimutaiandy@users.noreply.github.com>
2023-11-27 09:56:52 +0300

    Update snake_game.py

--- snake_game.py
+++ snake_game.py
@@ -27,6 +27,13 @@ class Snake:
         self.head = pygame.Rect(self.x, self.y, BLOCK_SIZE, BLOCK_SIZE)
         self.body = [pygame.Rect(self.x-BLOCK_SIZE,self.y, BLOCK_SIZE, BLOCK_SIZE)]
         self.dead = False
+    def update(self):
+        self.body.append(self.head)
+        for i in range(len(self.body)-1):
+            self.body[i].x, self.body[i].y = self.body[i+1].x, self.body[i+1].y
+        self.head.x = self.xdir * BLOCK_SIZE
+        self.head.y = self.ydir * BLOCK_SIZE
+        self.body.remove(self.hand)
 drawGrid()
 snake = Snake()
 
